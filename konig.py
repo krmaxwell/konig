57e24097eb7796e2a0f3e02989d3b7525a085fd8
Kyle Maxwell <kylem@xwell.org>
2013-01-26 19:14:14 -0600

    First *real* commit

--- konig.py
+++ konig.py
@@ -1,20 +1,56 @@
 import ssdeep
 import networkx as nx
+import matplotlib.pyplot as plt
+
 import argparse
+import os
 
-def calculatehashes()
-def creategraph()
+def calculatehashes(directory):
+    ourhashes = {}
 
-def main():
+    # get list of all files in the directory
+    dirlist=os.listdir(directory)
+
+    # iterate through each file
+    for f in dirlist:
+        # calculate hash and store
+        ourhashes[f] = ssdeep.hash_from_file(directory+'/'+f)
+
+    return ourhashes
+
+def creategraph(fuzzyhashes, threshold=50)
+    G = nx.Graph()
+
+    # iterate over keys in fuzzyhashes
+    for k in fuzzyhashes.iterkeys():
+        # calculate similarity to all *remaining* hashes
+        # TODO: fix this so we don't repeat ourselves, this is way naive
+        for l in fuzzyhashes.iterkeys():
+            if (k != l):        # literally the ONLY optimization so far
+                sim = ssdeep.compare(ourhashes[k], ourhashes[l])
+                # if similarity is >= threshold, add it to the graph
+                if sim >= threshold:
+                    G.add_edge(k, l, weight=sim)
 
 if __name__ == "__main__":
+    # handle command-line stuff
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", help="Directory of files to be hashed", default=".")
+    # TODO: use this 
+    parser.add_argument("-t", "--threshold", help="Threshold for similarity", default=80)
+    args = parser.parse_args()
+
     # first calculate all the fuzzy hashes for the files in a directory
     # get this directory from a command-line argument
-    malhashes = calculatehashes(dir) 
+    print("Calculating fuzzy hashes for all files in %s...", args.directory)
+    malhashes = calculatehashes(args.directory) 
 
     # now use these hashes to create an undirected graph of relationships
     # above a given threshold
+    print("Creating graph structure for files with similarity >= 80...")
     malgraph = creategraph(malhashes, threshold=80)
 
     # we should draw the graph at this point for visualization
-    drawgraph(malgraph)
+    print("Preparing plot of graph structure...")
+    nx.draw_circular(malgraph, with_labels=False)
+    plt.show()
