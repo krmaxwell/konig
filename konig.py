ba556cd69c73e6050514eb55536dd603f97b5eba
Kyle Maxwell <kylem@xwell.org>
2013-01-26 21:16:05 -0600

    Added copyright and license information

--- konig.py
+++ konig.py
@@ -1,3 +1,18 @@
+# Copyright 2013 Kyle Maxwell
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import ssdeep
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -54,5 +69,5 @@ if __name__ == "__main__":
 
     # we should draw the graph at this point for visualization
     print("Preparing plot of graph structure...")
-    nx.draw_spring(malgraph, with_labels=False)
+    nx.draw_spring(malgraph, with_labels=True)
     plt.show()
